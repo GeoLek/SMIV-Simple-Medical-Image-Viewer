221e8b3353d73f800ba638feb2eccfc33c087cbd
GeoLek <gxlekkas@gmail.com>
2025-03-01 00:33:17 +0200

    Helper Functions

---
+++ utils.py
@@ -0,0 +1,6 @@
+from PIL import Image
+
+def save_as_png(img_array, output_path):
+    """ Save NumPy array as PNG image """
+    img = Image.fromarray(img_array.astype("uint8"))
+    img.save(output_path, format="PNG")
